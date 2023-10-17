a5a1248993a3e340428552a6a3d1401169297a66
stivemok <stivemok@gmail.com>
2023-10-17 05:34:55 -0700

    Wher can i learn python

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Python function that returns the list of school having a specific topic"""
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Find by topic"""
+    return mongo_collection.find({"topics": topic})
